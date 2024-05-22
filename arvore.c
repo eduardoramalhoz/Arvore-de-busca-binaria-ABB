65251ab326c81c47d8dede3f8b8c5524d4607112
Maria Clara G. <mariaclagouv@gmail.com>
2024-05-22 18:05:15 -0300

    feat: insert function

--- arvore.c
+++ arvore.c
@@ -7,8 +7,8 @@ typedef struct no {
     struct no *direita, *esquerda;
 } NoArv;
 
-NoArv * inserir_v1 (NoArv *raiz, int num) {
-    if(raiz == NULL) {
+/*NoArv* inserir_v1 (NoArv *raiz, int num) { //versão mais custosa, pois tem o retorno de um nó toda vez que utilizada
+    if(raiz == NULL) {//verifica se o conteudo é nulo
         NoArv *aux = malloc(sizeof(NoArv));
         aux->valor = num;
         aux->direita = NULL;
@@ -24,11 +24,46 @@ NoArv * inserir_v1 (NoArv *raiz, int num) {
         } 
         return raiz;
     }
+}*/
+
+void inserir(NoArv **raiz, int num) {
+    if(*raiz == NULL) {//verificando se o conteudo do ponteiro é nulo
+        *raiz = malloc(sizeof(NoArv));//altera o conteúdo da raiz direto da main
+        (*raiz)->valor = num;
+        (*raiz)->direita = NULL;
+        (*raiz)->esquerda = NULL;
+
+    } else {
+        if(num < (*raiz)->valor) {
+            inserir_v2(&((*raiz)->esquerda), num);
+        } else {
+            inserir_v2(&((*raiz)->direita), num);
+        }
+    }
 }
+
+/*void inserir_v3 (NoArv **raiz, int num) {
+    NoArv *aux = *raiz;
+
+    while(aux) {
+        if(num < aux->valor) {
+            raiz = &aux->esquerda;
+        } else {
+            raiz = &aux->direita;
+        }
+        aux = *raiz;
+    }
+
+    aux = malloc(sizeof(NoArv));
+    aux->valor = num;
+    aux->direita = NULL;
+    aux->esquerda = NULL;
+    *raiz = aux;
+}*/
 
 void mostrar_v1 (NoArv *raiz) { // Mostra a arvore em ordem de insercao
     if(raiz) {
-        printf("%d ", raiz->valor);
+        printf("\t%d ", raiz->valor);
         mostrar_v1(raiz->esquerda);
         mostrar_v1(raiz->direita);
     }
@@ -37,7 +72,7 @@ void mostrar_v1 (NoArv *raiz) { // Mostra a arvore em ordem de insercao
 void mostrar_v2 (NoArv *raiz) { //mostra arvore ordenada
     if(raiz) {
         mostrar_v2(raiz->esquerda);
-        printf("%d ", raiz->valor);
+        printf("\t%d ", raiz->valor);
         mostrar_v2(raiz->direita);
     }
 }
@@ -55,7 +90,9 @@ int main () {
             case 1:
                 printf("\n\tDigite um valor:");
                 scanf("%d",&valor);
-                raiz = inserir_v1(raiz,valor);
+                //raiz = inserir_v1(raiz,valor);
+                inserir(&raiz, valor);//o & pega o endereço da variavel raiz, como a variavel raiz é um ponteiro, da certinho com o parametro **raiz
+                //inserir_v3(&raiz, valor);
                 break;
             case 2:
                 printf("\n\tPrimeira impressao:\n");
